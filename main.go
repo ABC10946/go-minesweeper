f0b42c750b1d007504301d48ad15e7d38f434710
ABC10946 <abctech10946@gmail.com>
2025-11-16 17:49:36 +0900

    feat: マインスイーパーのゲームロジックを改善
    - ゲームの更新処理にフラグ機能を追加し、右クリックでフラグを立てることができるようにしました。
    - 描画処理を修正し、セルの状態に応じた色の変更を実装しました。
    - ゲームオーバーとゲームクリアの状態を管理するロジックを追加しました。
    - セルの周囲の爆弾の数をカウントする機能を実装しました。
    - ゲームの初期化時にフィールドサイズを調整しました。

--- main.go
+++ main.go
@@ -4,52 +4,99 @@ import (
 	"fmt"
 	"image/color"
 	"log"
+	"strconv"
 
 	"github.com/ABC10946/minesweeper/minesweeperlogic"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type Game struct {
-	ms minesweeperlogic.MineSweeper
+	cellSize int
+	ms       minesweeperlogic.MineSweeper
 }
 
 func (g *Game) Update() error {
+	x, y := ebiten.CursorPosition()
+	flagPressed := inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonRight)
+	size := g.cellSize
+
+	for i := 0; i < g.ms.FieldHeight; i++ {
+		for j := 0; j < g.ms.FieldWidth; j++ {
+			if i*size < x && x < (i+1)*size && j*size < y && y < (j+1)*size {
+				if flagPressed {
+					g.ms.Flag(i, j)
+					fmt.Printf("FLAG PRESSED %v\n", g.ms.Field[j][i].Flag)
+				}
+			}
+		}
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	x, y := ebiten.CursorPosition()
 	mousePressed := ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft)
+	flagPressed := ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight)
 
-	size := 16
+	size := g.cellSize
+
 	for i := 0; i < g.ms.FieldHeight; i++ {
 		for j := 0; j < g.ms.FieldWidth; j++ {
 
 			if i*size < x && x < (i+1)*size && j*size < y && y < (j+1)*size {
 				if mousePressed {
 					g.drawRectAngle(screen, i*size, j*size, float32(size), color.RGBA{0x00, 0xff, 0x00, 0xff})
-					g.drawLineRectAngle(screen, i*size, j*size, float32(size), color.Black, 1)
-
-				} else {
+					g.drawLineRectAngle(screen, i*size, j*size, float32(size), color.White, 1)
+					g.ms.Open(i, j)
+				} else if flagPressed {
 					g.drawRectAngle(screen, i*size, j*size, float32(size), color.RGBA{0xff, 0xff, 0x00, 0xff})
-					g.drawLineRectAngle(screen, i*size, j*size, float32(size), color.Black, 1)
+					g.drawLineRectAngle(screen, i*size, j*size, float32(size), color.White, 1)
+				} else {
+					g.drawRectAngle(screen, i*size, j*size, float32(size), color.RGBA{0xff, 0x00, 0xff, 0xff})
+					g.drawLineRectAngle(screen, i*size, j*size, float32(size), color.White, 1)
 				}
 			} else {
-				if g.ms.Field[j][i].Bomb {
-					g.drawRectAngle(screen, i*size, j*size, float32(size), color.RGBA{0xff, 0x00, 0x00, 0xff})
+				// normal display process
+				if g.ms.Field[j][i].Open {
+					if g.ms.Field[j][i].Bomb {
+						g.drawRectAngle(screen, i*size, j*size, float32(size), color.RGBA{0xff, 0x00, 0x00, 0xff})
+						g.drawLineRectAngle(screen, i*size, j*size, float32(size), color.White, 1)
 
+					} else {
+						g.drawRectAngle(screen, i*size, j*size, float32(size), color.Black)
+						g.drawLineRectAngle(screen, i*size, j*size, float32(size), color.White, 1)
+					}
+					ebitenutil.DebugPrintAt(screen, strconv.Itoa(g.ms.Field[j][i].Count), i*size, j*size)
+				} else if g.ms.Field[j][i].Flag {
+					g.drawRectAngle(screen, i*size, j*size, float32(size), color.RGBA{0xff, 0xff, 0x00, 0xff})
+					g.drawLineRectAngle(screen, i*size, j*size, float32(size), color.White, 1)
 				} else {
-					g.drawRectAngle(screen, i*size, j*size, float32(size), color.White)
-					g.drawLineRectAngle(screen, i*size, j*size, float32(size), color.Black, 1)
+					g.drawLineRectAngle(screen, i*size, j*size, float32(size), color.White, 1)
+
 				}
 			}
+
 		}
+	}
+
+	g.ms.IsGameClear()
+
+	if g.ms.GameOver {
+		ebitenutil.DebugPrint(screen, "Game Over!")
+		g.ms.AllOpen()
+	}
+
+	if g.ms.GameClear {
+		ebitenutil.DebugPrint(screen, "Game Clear!")
 	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 320, 320
 }
 
 func (g *Game) drawRectAngle(screen *ebiten.Image, x, y int, size float32, colorData color.Color) {
@@ -96,11 +143,13 @@ func (g *Game) drawLineRectAngle(screen *ebiten.Image, x, y int, size float32, c
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 640)
+	ebiten.SetWindowSize(1280, 1280)
 	ebiten.SetWindowTitle("Hello world!")
 	game := Game{}
-	game.ms.Init(20, 20)
+	game.cellSize = 32
+	game.ms.Init(10, 10)
 	game.ms.SummonBomb()
+	game.ms.CountBomb()
 	fmt.Println(game.ms.Field)
 	if err := ebiten.RunGame(&game); err != nil {
 		log.Fatal(err)
--- minesweeperlogic/minesweeperlogic.go
+++ minesweeperlogic/minesweeperlogic.go
@@ -6,12 +6,16 @@ type Cell struct {
 	Open  bool
 	Count int
 	Bomb  bool
+	Flag  bool
 }
 
 type MineSweeper struct {
 	FieldWidth  int
 	FieldHeight int
 	Field       [][]Cell
+	GameOver    bool
+	TotalBomb   int
+	GameClear   bool
 }
 
 func (ms *MineSweeper) Init(fieldWidth int, fieldHeight int) {
@@ -26,18 +30,118 @@ func (ms *MineSweeper) Init(fieldWidth int, fieldHeight int) {
 }
 
 func (ms *MineSweeper) SummonBomb() {
+	totalBomb := 0
 	if ms.Field == nil {
 		panic("Error: field is empty")
 	}
 
-	fieldHeight := len(ms.Field)
-	fieldWidth := len(ms.Field[0])
-
-	for i := 0; i < fieldHeight; i++ {
-		for j := 0; j < fieldWidth; j++ {
+	for i := 0; i < ms.FieldHeight; i++ {
+		for j := 0; j < ms.FieldWidth; j++ {
 			if rand.NormFloat64() > 0.9 {
 				ms.Field[j][i].Bomb = true
+				totalBomb++
 			}
 		}
+	}
+
+	ms.TotalBomb = totalBomb
+}
+
+func (ms *MineSweeper) cellCountBomb(x, y int) int {
+	if ms.Field[y][x].Bomb {
+		return -1
+	}
+
+	// x, y
+	neighborIdx := [][]int{
+		{-1, -1},
+		{0, -1},
+		{1, -1},
+		{-1, 0},
+		{1, 0},
+		{-1, 1},
+		{0, 1},
+		{1, 1},
+	}
+
+	count := 0
+
+	for i := 0; i < 8; i++ {
+		xshift := neighborIdx[i][0]
+		yshift := neighborIdx[i][1]
+		neighborCellX := x + xshift
+		neighborCellY := y + yshift
+
+		if !ms.isOverWall(neighborCellX, neighborCellY) {
+			if ms.Field[neighborCellY][neighborCellX].Bomb {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+func (ms *MineSweeper) CountBomb() {
+	for i := 0; i < ms.FieldHeight; i++ {
+		for j := 0; j < ms.FieldWidth; j++ {
+			ms.Field[j][i].Count = ms.cellCountBomb(i, j)
+		}
+	}
+}
+
+func (ms *MineSweeper) isOverWall(x, y int) bool {
+	if x < 0 || y < 0 {
+		return true
+	}
+
+	if x >= ms.FieldWidth {
+		return true
+	}
+
+	if y >= ms.FieldHeight {
+		return true
+	}
+
+	return false
+}
+
+func (ms *MineSweeper) Open(x, y int) {
+	if !ms.Field[y][x].Flag {
+		if ms.Field[y][x].Bomb {
+			ms.GameOver = true
+		}
+
+		if !ms.Field[y][x].Bomb {
+			ms.Field[y][x].Open = true
+		}
+	}
+}
+
+func (ms *MineSweeper) AllOpen() {
+	for i := 0; i < ms.FieldHeight; i++ {
+		for j := 0; j < ms.FieldWidth; j++ {
+			ms.Field[j][i].Open = true
+		}
+	}
+}
+
+func (ms *MineSweeper) Flag(x, y int) {
+	ms.Field[y][x].Flag = !ms.Field[y][x].Flag
+}
+
+func (ms *MineSweeper) IsGameClear() {
+	emptyCellCount := ms.FieldHeight*ms.FieldWidth - ms.TotalBomb
+	openedCellCount := 0
+
+	for i := 0; i < ms.FieldHeight; i++ {
+		for j := 0; j < ms.FieldWidth; j++ {
+			if ms.Field[j][i].Open {
+				openedCellCount++
+			}
+		}
+	}
+	if openedCellCount == emptyCellCount {
+		ms.GameClear = true
 	}
 }
